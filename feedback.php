33bf7150b44dbb85cfa3a99adb773574522bca6c
Lover0301 <isip_d.v.drebencov@mpt.ru>
2022-12-18 22:16:48 +0300

    Обратная связь(нужен дизайн)

--- feedback.php
+++ feedback.php
@@ -2,8 +2,8 @@
     session_start();
     if(isset($_POST["FIO"]) && isset($_POST["telephone"]) && isset($_POST["opisanie"])):
         require "db.php";
-        $date = date("y.m.d H.m.s");
-        $sql = "INSERT INTO feedback (FIO, Phone_number, Opisanie, Datetime) VALUES (:FIO, :telephone, :opisanie, :segodna)";
+        $date = date("y.m.d");
+        $sql = "INSERT INTO feedback (FIO, Phone_number, Opisanie, Date) VALUES (:FIO, :telephone, :opisanie, :segodna)";
         $query = $pdo->prepare($sql);
         $query->execute(array(
             "FIO" => $_POST["FIO"],
