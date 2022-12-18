220dd9e746bcdaad724e506ad75a99920b26092a
Lover0301 <isip_d.v.drebencov@mpt.ru>
2022-12-18 22:25:07 +0300

    Подключение к БД

--- db.php
+++ db.php
@@ -5,7 +5,7 @@
     $username = 'root';
     $password = '';
     try {
-        $pdo= new PDO("mysql:host=$host;dbname=$dbname", $username, $password);
+        $pdo= new PDO("mysql:host=$host;dbname=$dbname", $username, $password,'utf8');
     } catch (Exception $pe) {
         echo "Ошибка";
     }
