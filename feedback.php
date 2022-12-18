677323edc75c54342de7a1c983720a0bac0249da
Lover0301 <isip_d.v.drebencov@mpt.ru>
2022-12-18 23:41:54 +0300

    Обратная связь (нужен дизайн)

--- feedback.php
+++ feedback.php
@@ -15,7 +15,7 @@
     else:
 ?>
 <!DOCTYPE html>
-<html>
+<html lang="RU">
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
