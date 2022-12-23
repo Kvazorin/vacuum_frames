3501e253f60cdc63b432a36e75819e27e311f61b
Kvazorin <povaroww@gmail.com>
2022-12-23 22:29:01 +0300

    Seventh commit
    - Added slick-slider;
    - Finished feedback section and footer;

---
+++ assets/js/script.js
@@ -0,0 +1,12 @@
+$('.section_recent-jobs__slider').slick({
+    slidesToShow: 1,
+    slidesToScroll: 3,
+    autoplay: true,
+    variableWidth: true,
+    autoplaySpeed: 3500,
+    prevArrow:'<span class="recent-jobs__slider_prev"></span>',
+    nextArrow: '<span class="recent-jobs__slider_next"></span>',
+    centerMode: true,
+    dots: true
+    
+});
