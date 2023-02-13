0ec28e38ffaa35ecf25c490663fafa20ab1118b8
Kvazorin <povaroww@gmail.com>
2023-02-13 14:21:48 +0300

    Eighth commit
    - Added designer page with collapsible options;
    - Added another feedback inputs (gotta change them a bit in the future);

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,18 @@
+var coll = document.getElementsByClassName("section__collapsible-btn");
+var i;
+
+for (i = 0; i < coll.length; i++) {
+  coll[i].addEventListener("click", function() {
+    this.classList.toggle("section__collapsible-btn_active");
+    var content = this.nextElementSibling;
+    if (content.style.maxHeight){
+      content.style.maxHeight = null;
+    } else {
+      content.style.maxHeight = content.scrollHeight + "px";
+    } 
+  });
+}
+
 $('.section_recent-jobs__slider').slick({
     slidesToShow: 1,
     slidesToScroll: 3,
@@ -10,3 +25,4 @@ $('.section_recent-jobs__slider').slick({
     dots: true
     
 });
+
